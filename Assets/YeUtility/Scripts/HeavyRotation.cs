5d65c9d7e3a7ac3831b18ecd8b92300fe3e4b9fa
georgehuang <ka1ss2ps@gmail.com>
2023-09-23 10:46:03 +0800

    Add HeavyRoationUpdater

--- Assets/YeUtility/Scripts/HeavyRotation.cs
+++ Assets/YeUtility/Scripts/HeavyRotation.cs
@@ -4,7 +4,6 @@ namespace YeUtility
 {
     public class HeavyRotation : MonoBehaviour
     {
-        public bool m_fixedUpdate = false;
         public bool m_useRotateCenter = false;
         public Vector3 m_speed = Vector3.zero;
         public Vector3 m_rotateCenter = Vector3.zero;
@@ -22,42 +21,20 @@ namespace YeUtility
             m_transCache = transform;
         }
 
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (m_fixedUpdate == false)
-            {
-                UpdateRotation();
-            }
-        }
-
-        void FixedUpdate()
-        {
-            if (m_fixedUpdate == true)
-            {
-                UpdateRotation();
-            }
-        }
 
         bool HasValue()
         {
             return m_speed.sqrMagnitude > 0;
         }
 
-        void UpdateRotation()
+        public void UpdateRotation(float dt)
         {
             if (HasValue() == false || m_pause == true)
                 return;
             if (m_useRotateCenter == false)
             {
                 //m_transCache.localEulerAngles = m_transCache.localEulerAngles + m_speed;
-                m_transCache.Rotate(m_speed * Time.deltaTime);
+                m_transCache.Rotate(m_speed * dt);
             }
             else
             {
---
+++ Assets/YeUtility/Scripts/HeavyRotationUpdater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace YeUtility.Scripts
+{
+    [RequireComponent(typeof(HeavyRotation))]
+    public class HeavyRotationUpdater : MonoBehaviour
+    {
+        [SerializeField] private HeavyRotation heavyRotation;
+
+        private void Update()
+        {
+            heavyRotation.UpdateRotation(Time.deltaTime);
+        }
+    }
+}
