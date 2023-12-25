cb069175b56cd66b2ddeea46564ef70a570995b8
Meader <ka1ss2ps@gmail.com>
2023-12-25 16:12:28 +0800

    HeaveyRotaionUpdater Add AutoGet

--- Assets/YeUtility/Scripts/YeHeavyRotationUpdater.cs
+++ Assets/YeUtility/Scripts/YeHeavyRotationUpdater.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace YeUtility
@@ -6,12 +7,20 @@ namespace YeUtility
     [RequireComponent(typeof(YeHeavyRotation))]
     public class YeHeavyRotationUpdater : MonoBehaviour
     {
-        [FormerlySerializedAs("heavyRotation")] [SerializeField]
-        private YeHeavyRotation yeHeavyRotation;
+        [SerializeField] private bool autoGet = true;
+        [SerializeField] private YeHeavyRotation yeHeavyRotation;
+        private void Awake()
+        {
+            if (yeHeavyRotation == null || autoGet)
+            {
+                yeHeavyRotation = GetComponent<YeHeavyRotation>();
+            }
+            
+        }
 
         private void Update()
         {
-            yeHeavyRotation.UpdateRotation(Time.deltaTime);
+            yeHeavyRotation?.UpdateRotation(Time.deltaTime);
         }
     }
 }
